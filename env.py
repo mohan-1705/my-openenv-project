8ee3699e1ebb28f65e20ec296d00eba1edb8ff96
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2026-04-01 16:45:40 +0530

    Rename SimpleEnv to MyEnv and modify step method
    Refactor SimpleEnv class to MyEnv and simplify step method.

--- env.py
+++ env.py
@@ -1,38 +1,13 @@
-class SimpleEnv:
+class MyEnv:
     def __init__(self):
         self.state = 0
-        self.max_state = 10
 
     def reset(self):
         self.state = 0
         return {"state": self.state}
 
-    def step(self, action=None):
-        # move forward
+    def step(self):
         self.state += 1
-
-        done = self.state >= self.max_state
-
-        #  Reward logic 
-        reward = self.state / self.max_state  # progress-based reward
-
-        return {
-            "state": {"state": self.state},
-            "reward": reward,
-            "done": done
-        }
-
-    #  TASKS 
-    def get_tasks(self):
-        return [
-            {"name": "easy", "goal": 3},
-            {"name": "medium", "goal": 6},
-            {"name": "hard", "goal": 10}
-        ]
-
-    #  GRADER 
-    def grade(self, final_state, goal):
-        if final_state >= goal:
-            return 1.0
-        else:
-            return final_state / goal
+        reward = 1
+        done = self.state >= 5
+        return {"state": self.state}, reward, done, {}
